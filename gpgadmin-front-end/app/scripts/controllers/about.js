880a8bf0971dab2f4003b2a83b8f8a7986d434ec
andmille <andmille@redhat.com>
2015-02-25 17:00:28 -0500

    Adding mockups.

---
+++ gpgadmin-front-end/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gpgadminFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gpgadminFrontEndApp
+ */
+angular.module('gpgadminFrontEndApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
