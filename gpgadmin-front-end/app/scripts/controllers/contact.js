46f176f112b2c44787d848deb839992b287db603
Jacob Oliver <joliver@redhat.com>
2015-02-27 14:41:33 -0500

    finish last commit

---
+++ gpgadmin-front-end/app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gpgadminFrontEndApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the gpgadminFrontEndApp
+ */
+angular.module('gpgadminFrontEndApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ gpgadmin-front-end/app/scripts/controllers/help.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gpgadminFrontEndApp.controller:HelpCtrl
+ * @description
+ * # HelpCtrl
+ * Controller of the gpgadminFrontEndApp
+ */
+angular.module('gpgadminFrontEndApp')
+  .controller('HelpCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
